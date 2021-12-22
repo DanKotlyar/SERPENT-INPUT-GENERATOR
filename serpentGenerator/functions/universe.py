1f375012914a381756caac5581a4d5f3b859571f
iaguirre6 <84925523+iaguirre6@users.noreply.github.com>
2021-12-21 20:15:10 -0500

    [DOC] universe.py documentation

--- serpentGenerator/functions/universe.py
+++ serpentGenerator/functions/universe.py
@@ -1,6 +1,6 @@
 """universe
 
-This class represents a universe, following from the tradtional serpent/mcnp universe
+This class represents a universe, following from the tradtional serpent universe
 based geometry.
 """
 
@@ -15,7 +15,6 @@ from serpentGenerator.functions.checkerrors import (
     _isstr, _isinstanceList
 )
 
-
 class universe:
     """Basic data definition for a universe obj.
 
@@ -28,7 +27,6 @@ class universe:
     cells : cells obj
         cells obj consisting of all cells making up the universe
     """
-    
     def __init__(self, id):
         _isstr(id, "universe id")
         self.id = id
@@ -36,6 +34,31 @@ class universe:
         self.surfs = sdict()
 
     def setGeom(self, surfs, cells):
+        """Assign surfaces and cells to a universe object.
+
+        The purpose of the ``setGeom`` function is to set the geometry for the 
+        universe object conisisting of surfaces and cells.
+
+        Parameters
+        ----------
+        surfs : list of surface obj
+            surfs obj consisting of all surfs making up the universe
+        cells : list of cell obj
+            cells obj consisting of all cells making up the universe
+
+        Raises
+        ------
+        TypeError
+            If ``surfs`` is not a list of surf objects
+            If ``cells`` is not a list of cell objects
+
+        Examples
+        --------
+        >>> universe1 = universe("u1")
+        >>> cells1 = [cells]
+        >>> surfs1 = [surfs]
+        >>> universe1.setGeom(surfs1, cells1)
+        """
         _isinstanceList(surfs, surf, "list of surface obj")
         _isinstanceList(cells, cell, "list of cell obj")
 
@@ -43,13 +66,55 @@ class universe:
         self.cells.addCells(cells)
 
     def toString(self):
+        """display properties of a universe in string form
+
+        The purpose of the ``toString`` function is to directly convert a universe
+        element into a string format for convinince when working with 
+        textfiles.
+
+        Returns
+        -------
+        str
+            universe in str format representing the typical input methodology for
+            input in serpent input file.
+        """
         for key in self.cells.cells:
             self.cells.cells[key].universe = self.id
 
         univString = self.surfs.toString() + self.cells.toString()
         return univString
     
-    def duplicate(self, newId):
+    def duplicateUniv(self, newId):
+        """returns a deep copy of the universe object must set a new universe id for
+         the new duplicated universe.
+
+        The purpose of the ``duplicate`` function is to return a deep copy of a 
+        universe object. The user must provide a new universe id for the copy, 
+        as universe ids must be unique. 
+
+        Parameters
+        ----------
+        newId : str
+            universe id to be set for the copy universe being returned.
+    
+        Returns
+        -------
+        newUniv : universe object
+            copy of the orginal universe object. 
+
+        Raises
+        ------
+        TypeError
+            If ``newId`` is not str.
+
+        Examples
+        --------
+        >>> universe1 = universe("u1")
+        >>> cells1 = [cells]
+        >>> surfs1 = [surfs]
+        >>> universe1.setGeom(surfs1, cells1)
+        >>> universe2 = universe1.duplicateUniv("u2)
+        """
         _isstr(newId, "new universe id")
         newUniv = copy.deepcopy(self)
         newUniv.id = newId
