a48559df23cd49e8b080a0de0ec70b8adf5f3de9
iaguirre6 <84925523+iaguirre6@users.noreply.github.com>
2021-12-21 21:23:05 -0500

    [DOC] mix.py documentation

--- serpentGenerator/functions/mix.py
+++ serpentGenerator/functions/mix.py
@@ -1,7 +1,7 @@
 """mix
 
 class representing a mix of materials weighted by mass fraction. Resembles the 
-serpent defnition of a mix. Often used when created borated water.
+serpent defnition of a mix. Often used when creating borated water.
 
 email: dan.kotlyar@me.gatech.edu
 email: iaguirre6@gatech.edu
@@ -14,9 +14,34 @@ from serpentGenerator.functions.checkerrors import (
 )
 
 class mix(material):
+    """Basic data definition for a material mix element.
+
+    Attributes
+    ----------
+    id : str
+        mix id/name
+    mats : list of materials objects
+        materials corresponding to mix
+    massfracs : list of floats
+        massfracs corresponding to materials in mix
+
+    Raises
+    ------
+    TypeError
+        If ``id`` is not str.
+        If ``mats`` is not a list of material objects.
+        If ``massfracs`` is not a list of numbers.
+    ValueError
+        If ``mats`` and ``massfracs`` are not of the same length.
+
+    Examples
+    --------
+    >>> mixmats = [water, boron]
+    >>> massfracs = [.999750, .000250]
+    >>> mix1 = mix("boratedWater, mixmats, massfracs)
+    """
     def __init__(self, id, mats, massfracs):
         _isstr(id, "mix id")
-        _islist(mats, "list of materials for mix")
         _isinstanceList(mats, material, "list of materials for mix")
         _isinstanceList(massfracs, numbers.Number , "list of massfracs for mix")
 
@@ -27,6 +52,18 @@ class mix(material):
         self.massfracs = massfracs
 
     def toString(self):
+        """display properties of a mix in string form
+
+        The purpose of the ``toString`` function is to directly convert a mix
+        element into a string format for convinince when working with 
+        textfiles.
+
+        Returns
+        -------
+        str
+            mix in str format representing the typical input methodology for
+            input in serpent input file.
+        """
         mixStr = "mix "+self.id+ "\n"
 
         for i in range(0, len(self.mats)):
