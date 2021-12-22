1a3b7d2b5d94ef819e415ee25ac355bd81041d75
iaguirre6 <84925523+iaguirre6@users.noreply.github.com>
2021-12-21 23:00:04 -0500

    [DEV] housing.py update
    [DEV] error checking implementation along with documentation

--- serpentGenerator/functions/housing.py
+++ serpentGenerator/functions/housing.py
@@ -6,25 +6,81 @@ email: dan.kotlyar@me.gatech.edu
 email: iaguirre6@gatech.edu
 """
 
-from serpentGenerator.data.materialLibrary import MATLIB
 from serpentGenerator.functions.material import material
 from serpentGenerator.functions.universe import universe
-from serpentGenerator.functions.surfs import surfs
 from serpentGenerator.functions.surf import surf
-from serpentGenerator.functions.cells import cells
 from serpentGenerator.functions.cell import cell
 
+from serpentGenerator.functions.checkerrors import (
+    _isnumber, _isbool, _isinstanceList
+)
+
 import numpy as np
-import copy 
+import numbers
+
 class housing(universe):
+    """Basic data definition for a housing bject. Has an option to set concentric
+    rings surrounding the reactor core, with specified materials and thicknesses.
+    This can be done by setting the ``defaultCRFlag`` to true.
+
+    Attributes
+    ----------
+    width : float
+        core width [cm]
+    height : bool
+        core hieght [cm]
+    defaultCRFlag : bool
+        True/False will use concentric ring option for core housing 
+    radiiCR : list of numbers
+        list containing radii of concentric rings, values are relative to core width
+    matsCR : list of material obj
+        list containing materials for each concentric shell surrounding core
+    border : TBD
+        TBD
+    balffle : TBD
+        TBD
+    barrel : TBD
+        TBD
+    thermalShield : TBD
+        TBD
+    neutronShield : TBD
+        TBD
+    vessel : TBD
+        TBD
+
+    Raises
+    ------
+    TypeError
+        If ``width``, ``height`` is not a number.
+        If ``defaultCRFlag`` is not a True/False.
+        If ``radiiCR``is not a list of numbers.
+        If ``matsCR``is not a list of material objects.
+    ValueError
+        If ``defaultCRFlag`` is set True, but ``radiiCR``, ``matsCR`` are None.
+
+    Examples
+    --------
+    >>> r1 = [0, 20, 40, 60]
+    >>> m1 = [MATLIB['Zr'], MATLIB['lightWater'], MATLIB['Zr']]
+    >>> h1 = housing(160, 240, defaultCRFlag=True,radiiCR=r1, matsCR=m1)
+    """
     def __init__(self, width, height, defaultCRFlag = True, radiiCR = None, 
     matsCR = None, border = None, baffle = None, barrel = None, thermalShield = None,
     neutronShield = None, vessel = None):
+        _isnumber(width, "core width")
+        _isnumber(height, "core height")
+        _isbool(defaultCRFlag, "Concentric Ring Flag")
+
         if (defaultCRFlag & ((radiiCR == None) | (matsCR == None))):
             raise ValueError("defaultCR flag set true but radiiCR"
             "or matsCR arguments not given.")
 
-        super().__init__("housing")
+        if not (isinstance(radiiCR, None)):
+            _isinstanceList(radiiCR, numbers.Number, "radii for concentric rings")
+        if not (isinstance(matsCR, None)):
+            _isinstanceList(matsCR, material, "materials for concentric rings")
+
+        super().__init__("housing") 
         self.border = border
         self.baffle = baffle
         self.barrel = barrel
@@ -43,7 +99,6 @@ class housing(universe):
             self._border(width)
 
     def _concRings(self, radii, mats):
-
         for i in range(0, len(radii)):
             radii[i] = radii[i] + self.width/2
 
@@ -60,8 +115,3 @@ class housing(universe):
         borderSurf = surf("border", "sqc", np.array([0.0, 0.0, width/2]))
         self.border = borderSurf
         self.surfs.addSurf(borderSurf)
-
-    def duplicate(self):
-        return copy.deepcopy(self)
-
-
